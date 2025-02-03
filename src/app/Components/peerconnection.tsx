cef86880c382d4f0ca9e0d593ad13c4483037d4a
Vyshnav-N-P <vyshnavnp666@gmail.com>
2025-02-03 11:56:13 +0530

    Updated Peerconnection page

--- src/app/Components/peerconnection.tsx
+++ src/app/Components/peerconnection.tsx
@@ -4,19 +4,47 @@ import '../globals.css';
 
 
 export default function Peerconnection(){
-    
+
     //Files
-    const [isconnected,setisconnected] =useState();
+    const [isconnected,setisconnected] =useState(true);
     const [sendFile,setsendfile] = useState();
     const [receiveFile,setreceivefile] = useState();
 
     //Channels and Connections
 
-    let localConnection = useRef(null);
-    let remoteConnection = useRef(null);
-    let sendchannel = useRef(null);
-    let receivechannel = useRef(null);
+    let localConnection = useRef <RTCPeerConnection | null> (null);
+    let remoteConnection = useRef <RTCPeerConnection | null> (null);
+    let sendchannel = useRef <RTCDataChannel | null> (null);
+    let receivechannel = useRef< RTCDataChannel | null> (null);
     
+    //Functions
+
+    const connect = async ()=>{
+        // Creating peer connection
+        localConnection.current = new RTCPeerConnection();
+        remoteConnection.current = new RTCPeerConnection();
+
+        // Creating data channels
+        sendchannel.current = localConnection.current.createDataChannel('sendchannel');
+        receivechannel.current = remoteConnection.current.createDataChannel('receivechannel');
+
+        sendchannel.current.onopen = () => {setisconnected(true)}
+        sendchannel.current.onclose = () => {setisconnected(false)}
+
+    };
+    
+    const disconnect = ()=>{
+        localConnection.current?.close();
+        remoteConnection.current?.close();
+        sendchannel.current?.close();
+        receivechannel.current?.close();
+
+        sendchannel.current=null;
+        receivechannel.current=null;
+        localConnection.current=null;
+        remoteConnection.current=null;
+        setisconnected(false);
+    };
     return (
         <>
 
