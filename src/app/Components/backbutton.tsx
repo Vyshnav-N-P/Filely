fac045f16798b5910efe5b8e56df373a5a9795a4
Vyshnav-N-P <vyshnavnp666@gmail.com>
2025-02-03 13:44:48 +0530

    Added a back button for UI

---
+++ src/app/Components/backbutton.tsx
@@ -0,0 +1,12 @@
+import BackIcon from '../../../public/Images/Back-icon.jpg'
+
+export default function Backbutton(){
+    return (
+    <div className='absolute left-3 top-5 text-sm'>
+        <a href="/">
+        <img src={BackIcon.src} alt="Back" className='object-cover max-w-10 max-h-10 hover:opacity-50' />
+        </a>
+    </div>
+    );
+};        
+        
--- src/app/contribute/page.tsx
+++ src/app/contribute/page.tsx
@@ -1,10 +1,15 @@
 import '../globals.css'
+import BackButton from '../Components/backbutton';
 
 export default function Contribute(){
     return (
         <>
-        <div>
-            <h1>Contribute</h1>
+        <BackButton />
+        <div className='flex flex-col justify-center align-middle items-center m-5'>
+            <h1 className='text-3xl text-white font-bold mb-10'>Contribute</h1>
+            <p className='text-lg text-white'>Help us grow by sharing this website with your peers</p>
+            <p className='text-lg text-white'>Do you have issues or perhaps ideas to improve the platform?
+            Words of feedback are very welcome!</p>
         </div>
         </>
     );
