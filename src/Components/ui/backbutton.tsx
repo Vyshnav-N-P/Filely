fc7140137dca439c74ff9252e37ece1dc0069923
Vyshnav-N-P <vyshnavnp666@gmail.com>
2025-02-14 21:53:21 +0530

    cleaning code for deploying

--- src/Components/ui/backbutton.tsx
+++ src/Components/ui/backbutton.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
+import Image from 'next/image';
 import BackIcon from '../../../public/Images/Back-icon.jpg'
 
 export default function Backbutton(){
     return (
     <div className='absolute left-3 top-5 text-sm'>
         <a href="/">
-        <img src={BackIcon.src} alt="Back" className='object-cover max-w-10 max-h-10 hover:opacity-50' />
+        <Image src={BackIcon.src} alt="Back" className='object-cover max-w-10 max-h-10 hover:opacity-50' />
         </a>
     </div>
     );
