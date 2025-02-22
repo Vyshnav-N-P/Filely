db8a19d61302e8d572daff2c8462fb2f6f32d4b0
Vyshnav-N-P <vyshnavnp666@gmail.com>
2025-02-22 11:25:00 +0530

    OR code generator

--- src/Components/features/generateQR.tsx
+++ src/Components/features/generateQR.tsx
@@ -1,23 +1,27 @@
 'use client'
-
-import { QRCode } from "qrcode";
+import '../../app/globals.css'
+import QRCode from "qrcode";
 import { useState, useEffect } from "react";
 import Image from "next/image";
+import { useRoomStore } from "@/stores/roomStore";
 
 export default function QRCodeGenerator() {
     const [qrCode, setQRCode] = useState<string>('');
-
+    const roomid = useRoomStore(state => state.ID);
     useEffect(()=>{
+        const generateQRCode = async () => {
         try {
-            
-        } catch (error) {
-            
-        }
+            const url = await QRCode.toDataURL(`https://filely.netlify.app/connect?id=${roomid}`); // Change URL here
+            setQRCode(url);
+            }   catch (error) {
+            console.log(error);
+        }}
+        generateQRCode();
     },[])
     return(
-        <>
-        <Image src={qrCode}/>
-        
-        </>
+        <div className='flex flex-col justify-center items-center gap-4'>
+        <Image src={qrCode} alt="QR code "/>
+        <p>Scan to recieve</p>
+        </div>
     );
 }
